09f94245f2fa508270f6a5b79dbb891c8ac52c7f
laura <102051838+illogicalmarginalia@users.noreply.github.com>
2026-03-24 11:44:33 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Hello!", "v": "0.1" }
+    return { "msg": "Hello! I have changed the web page!", "v": "0.1" }
 
 
 @app.get("/items/{id}")
