f48e3c1cd73980472b952a6a4755d29208f9419c
illogicalmarginalia <cunningl@arcada.fi>
2026-03-25 15:01:33 +0200

    add IP json

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Now for something entirely different", "v": "0.1" }
+    return { "msg": "Now for something entirely different", "v": "0.2" }
 
+@app.get("/api/ip")
+def read_root(request: Request):
+    client_host = request.client.host
+    return {"ip": client_host}
 
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
