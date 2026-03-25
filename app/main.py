ec3ce0e2a5d90728ca095150e208c8100e5e5496
illogicalmarginalia <cunningl@arcada.fi>
2026-03-25 15:07:36 +0200

    html ip

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI, Request
-
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return { "msg": "Now for something entirely different", "v": "0.2" }
 
-@app.get("/api/ip")
+@app.get("/api/ip", response_class=HTMLResponse)
 def read_root(request: Request):
     client_host = request.client.host
-    return {"ip": client_host}
+    return f"<h1> ip: {client_host} </h1>"
 
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
