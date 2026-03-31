aae6ae81611ee2c6b34c373b17611db015434aa6
illogicalmarginalia <cunningl@arcada.fi>
2026-03-31 10:08:16 +0300

    update cmd

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = [
+   # "https://cunningl-rahti-tester-web-communication-and-databases.2.rahtiapp.fi",
+   # "http://localhost",
+   # "http://localhost:8080",
+   # "http://localhost:5050",
+   "*", #change in production
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
