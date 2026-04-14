028fab1296817e963b999bd9500c1590db55a00f
illogicalmarginalia <cunningl@arcada.fi>
2026-04-14 14:57:06 +0300

    format

--- app/db.py
+++ app/db.py
@@ -2,13 +2,18 @@ import os, psycopg
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+
 def get_conn():
-    return psycopg.connect(DATABASE_URL, autocommit=True, row_factory=psycopg.rows.dict_row)
+    return psycopg.connect(
+        DATABASE_URL, autocommit=True, row_factory=psycopg.rows.dict_row
+    )
+
 
 def create_schema():
     with get_conn() as conn, conn.cursor() as cur:
         # Create the schema
-        cur.execute("""
+        cur.execute(
+            """
             CREATE TABLE IF NOT EXISTS rooms(
                 id SERIAL PRIMARY KEY, 
                 room_number INT NOT NULL,
@@ -35,7 +40,6 @@ def create_schema():
             
             ALTER TABLE rooms ADD COLUMN IF NOT EXISTS room_type VARCHAR;
                     
-        """)
+        """
+        )
         # add columns
-
-        
--- app/main.py
+++ app/main.py
@@ -8,11 +8,11 @@ from datetime import date
 app = FastAPI()
 
 origins = [
-   # "https://cunningl-rahti-tester-web-communication-and-databases.2.rahtiapp.fi",
-   # "http://localhost",
-   # "http://localhost:8080",
-   # "http://localhost:5500",
-   "*", #change in production
+    # "https://cunningl-rahti-tester-web-communication-and-databases.2.rahtiapp.fi",
+    # "http://localhost",
+    # "http://localhost:8080",
+    # "http://localhost:5500",
+    "*",  # change in production
 ]
 
 app.add_middleware(
@@ -25,13 +25,13 @@ app.add_middleware(
 
 create_schema()
 
-#Create Pydantic Model
+
+# Create Pydantic Model
 class Booking(BaseModel):
     guest_id: int
     room_id: int
     date_from: date
     date_to: date
-    
 
 
 @app.get("/")
@@ -40,17 +40,19 @@ def read_root():
         cur.execute("SELECT 'hello postgres'")
         result = cur.fetchone()
 
-    return {"msg": "Now for something entirely different", "dbstatus": result }
+    return {"msg": "Now for something entirely different", "dbstatus": result}
+
 
 @app.get("/api/ip")
 def read_root(request: Request):
     return {"ip": request.client.host}
 
-#@app.get("/api/ip", response_class=HTMLResponse)
-#def read_root(request: Request):
+
+# @app.get("/api/ip", response_class=HTMLResponse)
+# def read_root(request: Request):
 #    client_host = request.client.host
 #    return f"<h1> ip: {client_host} </h1>"
-#if-statements
+# if-statements
 @app.get("/if/{term}")
 def if_test(term: str):
     msg = "Default msg"
@@ -60,39 +62,48 @@ def if_test(term: str):
         msg = "Hej! Hur mår du?"
     elif term == "moi":
         msg = "Moika!"
-    else: 
+    else:
         msg = f"I don't understand {term}"
     return {"msg": msg}
+
 
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
     return {"id": id, "q": q}
 
-#View Rooms
+
+# View Rooms
 @app.get("/api/rooms")
 def read_root(request: Request):
-    hotelRooms = [{"roomNumber": 14, "bedSize" : "Queen", "TV" : "Yes", "Bookable":"Yes"}, 
-                  {"roomNumber": 7, "bedSize" : "Twin", "TV" : "No", "Bookable":"Yes"},
-                  {"roomNumber": 17, "bedSize" : "King", "TV" : "Yes", "Bookable":"No"}]
+    hotelRooms = [
+        {"roomNumber": 14, "bedSize": "Queen", "TV": "Yes", "Bookable": "Yes"},
+        {"roomNumber": 7, "bedSize": "Twin", "TV": "No", "Bookable": "Yes"},
+        {"roomNumber": 17, "bedSize": "King", "TV": "Yes", "Bookable": "No"},
+    ]
     return {"hotelRooms": hotelRooms}
 
+
 @app.get("/rooms/{id}")
-def get_one_room(id: int): 
+def get_one_room(id: int):
     with get_conn() as conn, conn.cursor() as cur:
-        cur.execute("""
+        cur.execute(
+            """
             SELECT * 
             FROM rooms 
             WHERE id = %s
-        """, (id,)) # <- tuple, list is also fine: [id]
+        """,
+            (id,),
+        )  # <- tuple, list is also fine: [id]
         room = cur.fetchone()
     return room
 
-  
-#Create Booking
+
+# Create Booking
 @app.post("/api/bookings")
 def create_booking(booking: Booking):
     with get_conn() as conn, conn.cursor() as cur:
-            cur.execute("""
+        cur.execute(
+            """
                 INSERT INTO bookings (
                     room_id, 
                     guest_id,
@@ -101,15 +112,9 @@ def create_booking(booking: Booking):
                 ) VALUES (
                     %s, %s, %s, %s
                 ) RETURNING id
-            """, [
-                booking.room_id, 
-                booking.guest_id,
-                booking.date_from,
-                booking.date_to
-            ])
-            new_booking = cur.fetchone()
-            
-    return { 
-            "msg": "Booking created!", 
-            "id": new_booking['id']
-            }
+            """,
+            [booking.room_id, booking.guest_id, booking.date_from, booking.date_to],
+        )
+        new_booking = cur.fetchone()
+
+    return {"msg": "Booking created!", "id": new_booking["id"]}
